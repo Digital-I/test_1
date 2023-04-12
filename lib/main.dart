296d6b9a40200a2acff56acd9618faefab406201
Ilya Belov <redingsquirrel@gmail.com>
2023-04-13 00:35:33 +0300

    1. Создал асинхронную функцию получения json с помощью библиотеки http. 2. Создал класс _CardsUsers создает список пользователей. 3. Создал класс UserProfile страница пользователя с подробной информацией

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,102 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter First App'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
   final String title;
-
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyHomePage> createState() => _CardsUsers();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _CardsUsers extends State<MyHomePage> {
+  var text = "None";
+  List<dynamic> usersList = [];
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
+  @override
+  Widget build(BuildContext context) {
+    Future<List<dynamic>> future = getData();
+    future.then((data) {
+      debugPrint("Данные получены");
+      usersList = data;
+      //setState(() {});
     });
-  }
 
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("List of users"),
+      ),
+      body: ListView(
+        children: [
+          for (var user in usersList)
+            ListTile(
+              title: Text(user["username"]),
+              subtitle: Text(user["name"]),
+              onTap: (){
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => UserProfile(data: user))
+                );
+              },
+            )
+        ],
+      ),
+    );
+  }
+}
+
+
+class UserProfile extends StatelessWidget {
+  const UserProfile({super.key, required this.data});
+  final Map<String, dynamic> data;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text(data["username"]),
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+          children: [
+            Text("Name: ${data["name"]}"),
+            Text("Email: ${data["email"]}"),
+            Text("Phone: ${data["phone"]}"),
+            Text("WebSite: ${data["website"]}"),
+            Text("Work company: ${data["company"]["name"]}"),
+            Text("BS: ${data["company"]["bs"]}"),
+            Text("Catch phrase: ${data["company"]["catchPhrase"]}", style: const TextStyle(fontStyle: FontStyle.italic)),
+            Text("Phone: ${data["phone"]}"),
+            Text("Address: ${data["address"]["city"]} ${data["address"]["suite"]} ${data["address"]["street"]}"),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
     );
+  }
+}
+
+Future<List<dynamic>> getData() async {
+  var response = await http.get(Uri.https('jsonplaceholder.typicode.com', '/users'));
+  if (response.statusCode == 200) {
+    return convert.jsonDecode(response.body) as List<dynamic>;
+  } else {
+    throw Exception("No data");
   }
 }
